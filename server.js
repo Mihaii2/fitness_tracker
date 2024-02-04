a423056dc67a54360a70922b72560c4e96c26954
mihai <mihainegoita9@gmail.com>
2024-02-04 21:06:43 +0200

    Second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -28,8 +28,8 @@ const server = http.createServer((req, res) => {
   `;
 
   res.end(htmlResponse);
-});
+}).listen(80);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
