ef37b445c7116bb30144b21a35804745dc1b0a5a
EC2 Default User <ec2-user@ip-172-31-94-12.ec2.internal>
2024-02-06 15:38:12 +0000

    Made folders tidier

--- node/index.js
+++ node/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 80;
+const hostname = '127.0.0.1'
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
   };
 
   const getCaloriesBurned = () => {
-    // Logic to calculate calories burned based on steps
+    // Logic to calculate calories burned bed on steps
     const steps = getSteps();
     return steps * 0.05;
   };
@@ -28,8 +28,8 @@ const server = http.createServer((req, res) => {
   `;
 
   res.end(htmlResponse);
-}).listen(80);
+});
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
