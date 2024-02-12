42b7f95deaf38877f8ce0a63880ccf0fcde66c44
mihai <mihainegoita9@gmail.com>
2024-02-12 23:35:19 +0200

    Added front-end topbar and started main header work

--- node/index.js
+++ node/index.js
@@ -13,38 +13,47 @@ const server = http.createServer((req, res) => {
     res.end();
   }
   );
-  
   handle_request(req, res);
-
-  
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 function handle_request(req, res) {
-  
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, '../mainpage/index.html'), function(err, data) {
+  switch (req.url) {
+    case '/':
+      fs.readFile(path.join(__dirname, '../mainpage/index.html'), function(err, data) {
         if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
+          res.writeHead(500);
+          return res.end('Error loading index.html');
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
-    });
-} else if (req.url.startsWith('/assets/logo/svg/logo-color.svg')) {
-    fs.readFile(path.join(__dirname, '..', req.url), function(err, data) {
+      });
+      break;
+    case '/assets/logo/svg/logo-no-background.svg':
+      fs.readFile(path.join(__dirname, '..', req.url), function(err, data) {
         if (err) {
-            res.writeHead(500);
-            return res.end('Error loading ' + req.url);
+          res.writeHead(500);
+          return res.end('Error loading ' + req.url);
         }
         res.writeHead(200, {'Content-Type': 'image/svg+xml'});
         res.end(data);
-    });
-} else {
-    res.writeHead(404);
-    res.end('Not found');
-}
+      });
+      break;
+    case '/styles/css/styles.css':
+      fs.readFile(path.join(__dirname, '..', req.url), function(err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading ' + req.url);
+        }
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      });
+      break;
+    default:
+      res.writeHead(404);
+      res.end('Not found');
+  }
 
 }
